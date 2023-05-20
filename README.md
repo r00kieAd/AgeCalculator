78b163f28d763e35a8de348066dbaa2d4e7c86fe
Adhyatma Dwivedi <113773621+r00kieAd@users.noreply.github.com>
2023-05-20 20:10:06 +0530

    Added Web Files

---
+++ README.md
@@ -0,0 +1,102 @@
+<div align="center" id="top"> 
+  <img src="./.github/app.gif" alt="Age Calculator" />
+
+  &#xa0;
+
+  <!-- <a href="https://webdevelopment.netlify.app">Demo</a> -->
+</div>
+
+<h1 align="center">WebDevelopment</h1>
+
+<p align="center">
+  <img alt="Github top language" src="https://img.shields.io/github/languages/top/r00kieAd/webdevelopment?color=56BEB8">
+
+  <img alt="Github language count" src="https://img.shields.io/github/languages/count/r00kieAd/webdevelopment?color=56BEB8">
+
+  <img alt="Repository size" src="https://img.shields.io/github/repo-size/r00kieAd/webdevelopment?color=56BEB8">
+
+  <img alt="License" src="https://img.shields.io/github/license/r00kieAd/webdevelopment?color=56BEB8">
+
+  <!-- <img alt="Github issues" src="https://img.shields.io/github/issues/{{YOUR_GITHUB_USERNAME}}/webdevelopment?color=56BEB8" /> -->
+
+  <!-- <img alt="Github forks" src="https://img.shields.io/github/forks/{{YOUR_GITHUB_USERNAME}}/webdevelopment?color=56BEB8" /> -->
+
+  <!-- <img alt="Github stars" src="https://img.shields.io/github/stars/{{YOUR_GITHUB_USERNAME}}/webdevelopment?color=56BEB8" /> -->
+</p>
+
+<!-- Status -->
+
+<!-- <h4 align="center"> 
+	🚧  WebDevelopment 🚀 Under construction...  🚧
+</h4> 
+
+<hr> -->
+
+<p align="center">
+  <a href="#dart-about">About</a> &#xa0; | &#xa0; 
+  <a href="#sparkles-features">Features</a> &#xa0; | &#xa0;
+  <a href="#rocket-technologies">Technologies</a> &#xa0; | &#xa0;
+  <a href="#white_check_mark-requirements">Requirements</a> &#xa0; | &#xa0;
+  <a href="#checkered_flag-starting">Starting</a> &#xa0; | &#xa0;
+  <a href="#memo-license">License</a> &#xa0; | &#xa0;
+  <a href="https://github.com/{{YOUR_GITHUB_USERNAME}}" target="_blank">Author</a>
+</p>
+
+<br>
+
+## :dart: About ##
+
+This is a Age Calculator Web App. This Web app tells you your age as per your inputs in Years, Months and Days.
+
+## :sparkles: Features ##
+
+1. This App will ask you your birthdate or any date you choose to enter in DD MM YYYY format.
+2. For validation, it will first check if all the inputs are entered.
+3. After receiving inputs and upon clicking the submit button (purple down arrow), it will validate your inputs based on real date data.
+4. Error will be thrown if Day depending upon the month is not in range 1-28, 1-29, 1-30 or 1-31.
+5. Error will be thrown if Month is not in range 1-12.
+6. For Year, the preferred range is 1000 till last year.
+7. For Error, input boxes will be colored red and won't change their appearance until error is corrected.
+8. An Error message will be displayed depending upon the error below each input boxes.
+
+## :rocket: Technologies ##
+
+The following tools were used in this project:
+
+- [JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+- [HTML](https://www.w3schools.com/html/)
+- [CSS](https://www.w3schools.com/css/default.asp)
+- [Bootstrap 5](https://getbootstrap.com/docs/5.2/getting-started/introduction/)
+
+## :white_check_mark: Requirements ##
+
+No Extra Requirements
+
+## :checkered_flag: Starting ##
+
+```bash
+# Clone this project
+$ git clone https://github.com/r00kieAd/AgeCalculator
+
+# Access
+$ cd AgeCalculator
+
+# Install dependencies
+$ yarn
+
+# Run the project
+$ yarn start
+
+# The server will initialize in the <http://localhost:3000>
+```
+
+## :memo: License ##
+
+This project is under license from FrontEnd Mentor and Adhyatma Dwivedi.
+
+
+Made with :heart: by <a href="https://github.com/r00kieAd" target="_blank">Adhyatma Dwivedi</a>
+
+&#xa0;
+
+<a href="#top">Back to top</a>
---
+++ myScript.js
@@ -0,0 +1,149 @@
+var greenFlag = true;
+
+function buttonColor(flag) {
+    if (flag == 1) {
+        document.querySelector("Button").style.backgroundColor = "Black";
+    } else {
+        document.querySelector("Button").style.backgroundColor = "hsl(259, 100%, 65%)";
+    }
+}
+
+function boxColor(flag, e) {
+    const inputs = document.querySelectorAll(".error");
+    // inputs.forEach(element => {
+    //     var x = element.style.visibility;
+    //     if (x == "visible") {
+    //         greenFlag = false;
+    //         return;
+    //     }
+    // });
+    if (e.style.borderColor != 'rgb(255, 87, 87)') {
+        if (flag == 1) {
+            e.style.borderColor = "hsl(259, 100%, 65%)";
+        } else {
+            e.style.borderColor = "hsl(0, 0%, 86%)";
+        }
+    }
+}
+
+try {
+
+    function validate() {
+        let day = document.querySelector("#day").value;
+        let month = document.querySelector("#month").value;
+        let year = document.querySelector("#year").value;
+        let error = false;
+
+        var e = document.querySelectorAll(".error");
+        var c = document.querySelectorAll("input");
+
+        if (day === '' && month === '' && year === '') {
+            throwError("This field is required");
+            return;
+        } else {
+            for (let i = 0; i < 3; i++) {
+                e[i].style.visibility = "hidden";
+                c[i].style.borderColor = "hsl(0, 0%, 86%)";
+            }
+        }
+
+        if (day === '') {
+            throwError("This field is required", 0);
+            error = true;
+        }
+        if (month === '') {
+            throwError("This field is required", 1);
+            error = true;
+        }
+        if (year === '') {
+            throwError("This field is required", 2);
+            error = true;
+        }
+
+        if (error) {
+            return;
+        }
+
+        const date = new Date(year, month);
+        date.setDate(0);
+        const totalDays = date.getDate();
+
+
+        if (day > totalDays || day < 1) {
+            throwError("Enter a valid day", 0);
+            error = true;
+        } else {
+            e[0].style.visibility = "hidden";
+            c[0].style.borderColor = "hsl(0, 0%, 86%)";
+        }
+
+        if (month < 0 || month > 12) {
+            throwError("Enter a valid month", 1);
+            error = true;
+        } else {
+            e[1].style.visibility = "hidden";
+            c[1].style.borderColor = "hsl(0, 0%, 86%)";
+        }
+
+        if (year > 0 && year < 1000) {
+            throwError("min year: 1000", 2);
+            error = true;
+        } else if (year >= new Date().getFullYear()) {
+            throwError("must be in past", 2);
+            error = true;
+        } else {
+            e[2].style.visibility = "hidden";
+            c[2].style.borderColor = "hsl(0, 0%, 86%)";
+        }
+
+        if (error) {
+            return;
+        }
+
+        ageCalculator(year, month, day);
+    }
+
+    function throwError(msg, n) {
+        const e = document.querySelectorAll(".error");
+        const c = document.querySelectorAll("input");
+        if (n == undefined) {
+            for (let i = 0; i < 3; i++) {
+                e[i].style.visibility = "visible";
+                e[i].innerHTML = msg;
+                c[i].style.borderColor = "hsl(0, 100%, 67%)";
+            }
+        } else {
+            e[n].style.visibility = "visible";
+            e[n].innerHTML = msg;
+            c[n].style.borderColor = "hsl(0, 100%, 67%)"
+        }
+        document.querySelector("h1.yyyy").innerHTML = "-- ";
+        document.querySelector("h1.mm").innerText = "-- ";
+        document.querySelector("h1.dd").innerText = "-- ";
+    }
+
+    function ageCalculator(year, month, day) {
+        month -= 1;
+        var years = new Date().getFullYear() - year;
+        if (new Date().getMonth() >= month) {
+            var months = new Date().getMonth() - month;
+        } else {
+            var months = Math.abs(new Date().getMonth() - month);
+            years = years - 1;
+            months = 12 - months;
+        }
+        var days = new Date().getDate();
+        if (months == 0) {
+            if (days == day) {
+                days = 0;
+            } else if (days > day) {
+                days -= day;
+            }
+        }
+        document.querySelector("h1.yyyy").innerHTML = years + " ";
+        document.querySelector("h1.mm").innerText = months + " ";
+        document.querySelector("h1.dd").innerText = days + " ";
+    }
+} catch (err) {
+    alert(err.message);
+}
